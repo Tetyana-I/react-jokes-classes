e849e5539df16c8eec873ce2b35053568802d69b
Tetyana <iltaev2010@gmail.com>
2022-01-19 16:49:02 -0600

    JokeList started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import JokeList from "./JokeList";
+import JokeListClassBased from "./JokeListClassBased";
 
 function App() {
   return (
     <div className="App">
-      <JokeList />
+      <JokeListClassBased />
     </div>
   );
 }
